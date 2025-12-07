c1d5d065272364c424f41ba797a180ebd4270474
Harsha Vardhan <155301114+Harshavardhanchary@users.noreply.github.com>
2025-12-07 18:28:33 +0530

    Add AWS instance and security group for GitHub Actions
    Added a new AWS instance and security group for GitHub Actions.

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,5 +1,17 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0ecb62995f68bb549"
+resource "aws_instance" "through-github actions" {
+  ami           = "ami-02b8269d5e85954ef"
+  key_name      = "First-Key"
   instance_type = "m7i-flex.large"
 }
 
+resource "aws_security_group" "For-github-actions" {
+  name        = "Allow-SSH-HTTP"
+  description = "Security group for GitHub Actions instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
